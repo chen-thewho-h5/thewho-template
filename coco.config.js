b96955eb39368f109054016c1e9aa301d669be1a
thewho <3187138761@qq.com>
2022-08-04 21:43:04 +0800

    add

---
+++ coco.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'thewho-template',
+  templateName: '登陆页面',
+  author: 'thewho',
+  snapshot: 'https://cdn.img.snapshot.png',
+  gitUrl: 'chen-thewho-h5/thewho-template',
+  projectType: '0'
+};
